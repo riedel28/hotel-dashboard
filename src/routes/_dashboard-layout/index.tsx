788e5e5faf71f51d2511983e287f0bdf47acb810
Sergey Riedel <sergey.riedel@gmail.com>
2025-02-01 14:53:52 +0100

    refactor(style): change globals styles to zinc

--- src/routes/_dashboard-layout/index.tsx
+++ src/routes/_dashboard-layout/index.tsx
@@ -13,6 +13,33 @@ function StartPage() {
     <section className="grid gap-2 p-2">
       <p>Hi {auth.user?.email}!</p>
       <p>You are currently on the dashboard route.</p>
+
+      <div className="grid max-w-md grid-cols-2 gap-4 rounded-md border-muted bg-muted p-4">
+        <div className="flex flex-col gap-1">
+          <span className="text-sm font-medium text-muted-foreground">
+            Serial nummer
+          </span>
+          <span>12QW43TYOP</span>
+        </div>
+        <div className="flex flex-col gap-1">
+          <span className="text-sm font-medium text-muted-foreground">
+            MAC-Address
+          </span>
+          <span>123.01.05.85</span>
+        </div>
+        <div className="flex flex-col gap-1">
+          <span className="text-sm font-medium text-muted-foreground">
+            Device name
+          </span>
+          <span>iPad (3th generation)</span>
+        </div>
+        <div className="flex flex-col gap-1">
+          <span className="text-sm font-medium text-muted-foreground">
+            Last signal
+          </span>
+          <span>20.12.2024</span>
+        </div>
+      </div>
     </section>
   );
 }
