8bd9871ff9078c880194cd803e127ac13def7e20
Sergey Riedel <sergey.riedel@gmail.com>
2025-08-04 13:29:29 +0200

    feat(data-grid): implement drag-and-drop functionality and enhance pagination
    - Added drag-and-drop support for rows and columns in the DataGrid component using dnd-kit.
    - Introduced new components for data grid pagination, column visibility, and column filtering.
    - Enhanced internationalization with new keys for pagination and filter messages in English and German.
    - Improved overall structure and usability of the DataGrid, including better loading states and accessibility features.

---
+++ src/components/ui/data-grid-pagination.tsx
@@ -0,0 +1,249 @@
+import { ReactNode } from 'react';
+
+import { ChevronLeftIcon, ChevronRightIcon } from 'lucide-react';
+import { FormattedMessage } from 'react-intl';
+
+import { Button } from '@/components/ui/button';
+import { useDataGrid } from '@/components/ui/data-grid';
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue
+} from '@/components/ui/select';
+import { Skeleton } from '@/components/ui/skeleton';
+
+import { cn } from '@/lib/utils';
+
+interface DataGridPaginationProps {
+  sizes?: number[];
+  sizesInfo?: string;
+  sizesLabel?: string;
+  sizesDescription?: string;
+  sizesSkeleton?: ReactNode;
+  more?: boolean;
+  moreLimit?: number;
+  info?: string;
+  infoSkeleton?: ReactNode;
+  className?: string;
+}
+
+function DataGridPagination(props: DataGridPaginationProps) {
+  const { table, recordCount, isLoading } = useDataGrid();
+
+  const defaultProps: Partial<DataGridPaginationProps> = {
+    sizes: [5, 10, 25, 50, 100],
+    sizesLabel: 'pagination.show',
+    sizesDescription: 'pagination.perPage',
+    sizesSkeleton: <Skeleton className="h-8 w-44" />,
+    moreLimit: 5,
+    more: false,
+    info: 'pagination.info',
+    infoSkeleton: <Skeleton className="h-8 w-60" />
+  };
+
+  const mergedProps: DataGridPaginationProps = { ...defaultProps, ...props };
+
+  const btnBaseClasses = 'size-7 p-0 text-sm';
+  const btnArrowClasses = btnBaseClasses + ' rtl:transform rtl:rotate-180';
+  const pageIndex = table.getState().pagination.pageIndex;
+  const pageSize = table.getState().pagination.pageSize;
+  const from = pageIndex * pageSize + 1;
+  const to = Math.min((pageIndex + 1) * pageSize, recordCount);
+  const pageCount = table.getPageCount();
+
+  // Replace placeholders in paginationInfo
+  const paginationInfo = mergedProps?.info
+    ? mergedProps.info
+        .replace('{from}', from.toString())
+        .replace('{to}', to.toString())
+        .replace('{count}', recordCount.toString())
+    : `${from} - ${to} of ${recordCount}`;
+
+  // Pagination limit logic
+  const paginationMoreLimit = mergedProps?.moreLimit || 5;
+
+  // Determine the start and end of the pagination group
+  const currentGroupStart =
+    Math.floor(pageIndex / paginationMoreLimit) * paginationMoreLimit;
+  const currentGroupEnd = Math.min(
+    currentGroupStart + paginationMoreLimit,
+    pageCount
+  );
+
+  // Render page buttons based on the current group
+  const renderPageButtons = () => {
+    const buttons = [];
+    for (let i = currentGroupStart; i < currentGroupEnd; i++) {
+      buttons.push(
+        <Button
+          key={i}
+          size="sm"
+          mode="icon"
+          variant="ghost"
+          className={cn(btnBaseClasses, 'text-muted-foreground', {
+            'bg-accent text-accent-foreground': pageIndex === i
+          })}
+          onClick={() => {
+            if (pageIndex !== i) {
+              table.setPageIndex(i);
+            }
+          }}
+        >
+          {i + 1}
+        </Button>
+      );
+    }
+    return buttons;
+  };
+
+  // Render a "previous" ellipsis button if there are previous pages to show
+  const renderEllipsisPrevButton = () => {
+    if (currentGroupStart > 0) {
+      return (
+        <Button
+          size="sm"
+          mode="icon"
+          className={btnBaseClasses}
+          variant="ghost"
+          onClick={() => table.setPageIndex(currentGroupStart - 1)}
+        >
+          <FormattedMessage id="pagination.more" defaultMessage="..." />
+        </Button>
+      );
+    }
+    return null;
+  };
+
+  // Render a "next" ellipsis button if there are more pages to show after the current group
+  const renderEllipsisNextButton = () => {
+    if (currentGroupEnd < pageCount) {
+      return (
+        <Button
+          className={btnBaseClasses}
+          variant="ghost"
+          size="sm"
+          mode="icon"
+          onClick={() => table.setPageIndex(currentGroupEnd)}
+        >
+          <FormattedMessage id="pagination.more" defaultMessage="..." />
+        </Button>
+      );
+    }
+    return null;
+  };
+
+  return (
+    <div
+      data-slot="data-grid-pagination"
+      className={cn(
+        'flex grow flex-col flex-wrap items-center justify-between gap-2.5 py-2.5 sm:flex-row sm:py-0',
+        mergedProps?.className
+      )}
+    >
+      <div className="order-1 flex flex-col items-center justify-center gap-2.5 pt-2.5 sm:order-1 sm:flex-row sm:justify-end sm:pt-0">
+        {isLoading ? (
+          mergedProps?.infoSkeleton
+        ) : (
+          <>
+            <div className="text-muted-foreground order-2 text-sm text-nowrap sm:order-1">
+              {mergedProps?.info?.startsWith('pagination.') ? (
+                <FormattedMessage
+                  id={mergedProps.info}
+                  defaultMessage="{from} - {to} of {count}"
+                  values={{
+                    from,
+                    to,
+                    count: recordCount
+                  }}
+                />
+              ) : (
+                paginationInfo
+              )}
+            </div>
+            {pageCount > 1 && (
+              <div className="order-1 flex items-center space-x-1 sm:order-2">
+                <Button
+                  size="sm"
+                  mode="icon"
+                  variant="ghost"
+                  className={btnArrowClasses}
+                  onClick={() => table.previousPage()}
+                  disabled={!table.getCanPreviousPage()}
+                >
+                  <span className="sr-only">
+                    <FormattedMessage
+                      id="pagination.goToPreviousPage"
+                      defaultMessage="Go to previous page"
+                    />
+                  </span>
+                  <ChevronLeftIcon className="size-4" />
+                </Button>
+
+                {renderEllipsisPrevButton()}
+
+                {renderPageButtons()}
+
+                {renderEllipsisNextButton()}
+
+                <Button
+                  size="sm"
+                  mode="icon"
+                  variant="ghost"
+                  className={btnArrowClasses}
+                  onClick={() => table.nextPage()}
+                  disabled={!table.getCanNextPage()}
+                >
+                  <span className="sr-only">
+                    <FormattedMessage
+                      id="pagination.goToNextPage"
+                      defaultMessage="Go to next page"
+                    />
+                  </span>
+                  <ChevronRightIcon className="size-4" />
+                </Button>
+              </div>
+            )}
+          </>
+        )}
+      </div>
+      <div className="order-2 flex flex-wrap items-center space-x-2.5 pb-2.5 sm:order-2 sm:pb-0">
+        {isLoading ? (
+          mergedProps?.sizesSkeleton
+        ) : (
+          <>
+            <div className="text-muted-foreground text-sm">
+              <FormattedMessage
+                id="pagination.rowsPerPage"
+                defaultMessage="Rows per page"
+              />
+            </div>
+            <Select
+              value={`${pageSize}`}
+              indicatorPosition="right"
+              onValueChange={(value) => {
+                const newPageSize = Number(value);
+                table.setPageSize(newPageSize);
+              }}
+            >
+              <SelectTrigger className="w-fit" size="sm">
+                {/* eslint-disable-next-line formatjs/no-literal-string-in-jsx */}
+                <SelectValue placeholder={`${pageSize}`} />
+              </SelectTrigger>
+              <SelectContent side="top" className="min-w-[50px]">
+                {mergedProps?.sizes?.map((size: number) => (
+                  <SelectItem key={size} value={`${size}`}>
+                    {size}
+                  </SelectItem>
+                ))}
+              </SelectContent>
+            </Select>
+          </>
+        )}
+      </div>
+    </div>
+  );
+}
+
+export { DataGridPagination, type DataGridPaginationProps };
